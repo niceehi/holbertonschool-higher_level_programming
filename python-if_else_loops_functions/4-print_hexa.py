b1fb89eb96762aa0c57373a11108aa4bb8bd8cab
niceehi <yourislhfhfhislamm@gmail.com>
2026-03-21 22:09:22 +0000

    Task

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+for i in range(99):
+    k = i % 16
+
+    if k == 10:
+        c = 'a'
+    elif k == 11:
+        c = 'b'
+    elif k == 12:
+        c = 'c'
+    elif k == 13:
+        c = 'd'
+    elif k == 14:
+        c = 'e'
+    elif k == 15:
+        c = 'f'
+    else:
+        c = k
+
+    print(f"{i} = 0x{c}")
