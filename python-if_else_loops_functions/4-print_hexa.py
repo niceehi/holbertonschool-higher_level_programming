d2eb1e91f4115a35dde21386d798791b3f26978b
niceehi <yourislhfhfhislamm@gmail.com>
2026-03-21 22:23:08 +0000

    Task

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -3,19 +3,4 @@
 for i in range(99):
     k = i % 16
 
-    if k == 10:
-        c = 'a'
-    elif k == 11:
-        c = 'b'
-    elif k == 12:
-        c = 'c'
-    elif k == 13:
-        c = 'd'
-    elif k == 14:
-        c = 'e'
-    elif k == 15:
-        c = 'f'
-    else:
-        c = k
-
-    print(i, " = 0x{}".format(c))
+    print("{} = 0x{:x}".format(i, i))
