85f92816bff79676ca653c575099bc8bac2b1e3b
niceehi <hsnislam2026hello@gmail.com>
2026-03-27 10:06:35 +0000

    Task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    char = chr(i)
-    if (char != 'q' and char != 'e'):
-        print("{}".format(char), end="")
+for i in range(97, 123):
+    if (chr(i) != 'q' and chr(i) != 'e'):
+        print("{}".format(chr(i)), end="")
