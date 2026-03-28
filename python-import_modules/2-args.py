9e1599152942e854356a330b01c98746bd34ed77
niceehi <hsnislam2026hellom@gmail.com>
2026-03-28 22:17:56 +0000

    Task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,5 +2,15 @@
 from sys import argv
 
 if __name__ == "__main__":
-    print(len(argv)-1)
-    print(argv[1:])
+    args = argv[1:]
+    count = len(args)
+
+    if count == 0:
+        print("Number of arguments.")
+    elif count == 1:
+        print("Number of argument: 1")
+    else:
+        print(f"Number of arguments: {count}")
+
+    for i, arg in enumerate(args, 1):
+        print(f"{i}: {arg}")
