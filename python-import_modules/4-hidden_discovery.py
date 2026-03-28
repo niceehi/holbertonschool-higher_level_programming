6784a5fabfd70fc956451aaa1ad3f58bc8e0b6fb
niceehi <hsnislam2026hellom@gmail.com>
2026-03-28 23:37:34 +0000

    Task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4
-
     names = [name for name in dir(hidden_4) if not name.startswith("__")]
-
     for name in sorted(names):
         print(name)
+
