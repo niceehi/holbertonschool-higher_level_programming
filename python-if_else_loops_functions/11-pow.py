8fa01b36213fd3e33c84f5970f797e842fdcecb6
niceehi <hsnislam2026hellom@gmail.com>
2026-03-28 16:20:52 +0000

    Task

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    for i in range(b+1):
+    while b != 0:
         a = a * a
+        if b > 0:
+            b = b -1
+        else:
+            b = b + 1
     return a
