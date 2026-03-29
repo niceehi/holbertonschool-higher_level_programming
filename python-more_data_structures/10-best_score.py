420cf0d7c3f77f5d016e62a17bb32753ff598067
niceehi <hsnidlsma2026helo@gmail.com>
2026-03-29 19:44:36 +0000

    Task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,12 @@
 
 def best_score(a_dictionary):
     if not a_dictionary:
-        return None
+         return None
 
     b_key = None
     max_value = None
     for key, value in a_dictionary.items():
         if max_value is None  or value > max_value:
             max_value = value
-            b_key = key 
+            b_key = key
     return b_key
-        
