a347d9d924e82c519635a21735fed4e064ae4c01
niceehi <hsnidlsma2026helo@gmail.com>
2026-03-29 15:42:22 +0000

    Task

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     my_2 = []
-    for i in range:
+    for i in range(0, len(my_list)):
         my_2 [i] = my_list[i]
     my_2 [idx] = element
     return my_2
