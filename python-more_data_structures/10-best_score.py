a2944358cec33185dbbc5574532506888cea6e29
niceehi <hsnidlsma2026helo@gmail.com>
2026-03-29 19:44:55 +0000

    Task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     b_key = None
     max_value = None
     for key, value in a_dictionary.items():
-        if max_value is None  or value > max_value:
+        if max_value is None or value > max_value:
             max_value = value
             b_key = key
     return b_key
