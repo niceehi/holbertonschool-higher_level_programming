59eddf9f6fb3d61221046a7be236512dc9d934d4
niceehi <hsnidlsma2026helo@gmail.com>
2026-03-29 16:00:50 +0000

    Task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 new_string = []
 def no_c(my_string):
     for i in my_string:
-        if i != 'c':
+        if my_string [i] != 'c':
             new_string.append(my_string[i])
     return new_string
