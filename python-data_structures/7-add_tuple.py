df97c1afb0dc1885004d29b58eff6f29dded3185
niceehi <hsnislam2026helmmo@gmail.com>
2026-04-01 07:25:46 +0000

    Task

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    k = tuple_a[0] + tuple_b[0]
-    l = tuple_a[1] + tuple_b[1]
-    return k, l
+    if len(tuple_a[0]) > 0:
+        a1 = tuple_a[0]
+    else:
+        a1 = 0
+    if len(tuple_a[1]) > 0:
+        a2 = tuple_a[1]
+    else:
+        a2 = 0
+    if len(tuple_b[0]) > 0:
+        a3 = tuple_b[0]
+    else:
+        a3 = 0
+    if len(tuple_b[1]) > 0:
+        a4 = tuple_b[1]
+    else:
+        a4 = 0
