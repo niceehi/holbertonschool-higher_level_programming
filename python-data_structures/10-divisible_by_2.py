213898aa2e54e308c5f23a822454592f5d4c58a8
niceehi <hsnislam2026helmmo@gmail.com>
2026-04-01 08:28:19 +0000

    Task

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,6 +4,9 @@ def divisible_by_2(my_list=[]):
 
     new_list = []
     for i in my_list:
+        if i == 0:
+            new_list.append(False)
+            break
         if i % 2 == 0:
             new_list.append(True)
         else:
