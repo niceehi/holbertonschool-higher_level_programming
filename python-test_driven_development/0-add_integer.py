2c9b884371e8009f9a86746d486ffef5fb60aba4
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-10 12:52:06 +0400

    Task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""Module that provides a function to add two integers."""
+
 
 def add_integer(a, b=98):
+    """
+    Return the integer addition of a and b.
+
+    a and b must be int or float, otherwise raise TypeError.
+    Floats are cast to int before addition.
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int,float)):
+
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-
-    return int(a) + (b)
+    return int(a) + int(b)
