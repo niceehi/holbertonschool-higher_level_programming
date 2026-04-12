e2f2350bc2f8a0f60632e8223454c15945f87e6d
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-13 01:35:12 +0400

    Task

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,13 +2,15 @@
 """This module divides all elements of a matrix by a number."""
 
 def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by div and returns a new matrix."""
     new_matrix = []
 
     for row in matrix:
         new_row = []
-        for num in row:
-            new_row.append(round (num/2, 2))
 
-    new_matrix.append(new_row)
+        for num in row:
+            new_row.append(round(num / div, 2))
+
+        new_matrix.append(new_row)
 
     return new_matrix
