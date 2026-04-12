f6c1a70cf61a1ce587e70d102d0ff1bd96c84e42
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-13 01:46:30 +0400

    Task

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,19 @@
-#!/usr/bin/python3
-"""This module divides all elements of a matrix by a number."""
-
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by div and returns a new matrix."""
 
+    # ❗ 1. check empty FIRST (very important)
+    if matrix == [] or matrix == [[]]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # ❗ 2. check structure
     if (not isinstance(matrix, list) or
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(num, (int, float)) for row in matrix for num in row)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
+    # ❗ 3. ONLY NOW safe to use matrix[0]
     row_size = len(matrix[0])
+
     for row in matrix:
         if len(row) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(round(num / div, 2))
-        new_matrix.append(new_row)
-
-    return new_matrix
