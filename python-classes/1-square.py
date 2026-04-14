79e6667a0b0e1fdd07573d029db860775178f945
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-14 12:12:29 +0400

    Task

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize square with size."""
+        self.__size = size
