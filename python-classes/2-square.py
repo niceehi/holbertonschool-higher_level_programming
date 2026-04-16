dec63f346243295a3860ece12bde4bf1115b654d
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-16 22:20:25 +0400

    Task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,8 +9,8 @@ class Square:
         """Initialize square with size."""
         self.__size = size
 
-        if not  isinstance(self.__size, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
