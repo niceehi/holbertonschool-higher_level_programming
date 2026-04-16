307f333a73096104049ba3edb79ab64781f43774
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-17 00:51:27 +0400

    Task

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with size."""
+        self.__size = size
+
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return(self.__size * self.__size)
