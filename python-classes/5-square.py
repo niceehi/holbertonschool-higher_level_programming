01e3f4edb739299c501310bf587b66848ac244ff
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-04-18 20:47:17 +0400

    Task

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,23 +5,24 @@
 class Square:
     """A class that defines a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize square with optional size."""
         self.size = size
+        self.position = position
 
     @property
     def size(self):
         """Retrieve the size."""
         return self.__size
 
-    @size.setter
-    def size(self, value):
+    @position.setter
+    def position(self, value):
         """Set the size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__position = value
 
     def area(self):
         """Return the square area."""
@@ -35,3 +36,9 @@ class Square:
 
         for _ in range(self.__size):
             print("#" * self.__size)
+         
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__position[2]):
+            print(" " * self.__position[0] + "#" * self.__size)
