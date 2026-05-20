2a61cb59980239e69030691588e2a10b3ebecf9d
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-05-21 01:35:01 +0400

    Task

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -61,3 +61,8 @@ def add_user():
     users[username] = user_data
 
     return jsonify({"message": "User added", "user": user_data}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+"""debug=True for flask automatisation and detailed errors"""
