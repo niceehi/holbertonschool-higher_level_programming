3e1ed0509a8b3152a0e4e2b5c89f2758ecdf673d
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-05-21 01:36:10 +0400

    Task

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -49,7 +49,7 @@ def add_user():
         return jsonify({"error": "Username is required"}), 400
 
     if username in users:
-        return jsonify({"error": "Username already exists"}), 400
+        return jsonify({"error": "User already exists"}), 400
 
     user_data = {
         "username": username,
